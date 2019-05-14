f5d6d3c620a7875c303964eb7e3000bd5d4db70b
Aerol <kotetsky@gmail.com>
2019-05-14 22:53:25 +0300

    4 add database to the site Hibernate

--- src/main/java/com/spikart/sweater/GreetingController.java
+++ src/main/java/com/spikart/sweater/GreetingController.java
@@ -1,14 +1,23 @@
 package com.spikart.sweater;
 
 
+import com.spikart.sweater.domain.Message;
+import com.spikart.sweater.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -21,9 +30,37 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        String superString = "Super Girl !";
-        model.put("some", superString);
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
 
+    @PostMapping
+    public String add(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(
+            @RequestParam String filter,
+            Map<String, Object> model) {
+        Iterable<Message> messages = new ArrayList<>();
+        if (filter.isEmpty()) {
+            messages = messageRepository.findAll();
+        } else {
+            messages = messageRepository.findByTag(filter);
+        }
+        model.put("messages", messages);
+        return "main";
+    }
 }
--- src/main/java/com/spikart/sweater/repository/MessageRepository.java
+++ src/main/java/com/spikart/sweater/repository/MessageRepository.java
@@ -1,4 +1,11 @@
 package com.spikart.sweater.repository;
 
-public class MessageRepository {
+import com.spikart.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
